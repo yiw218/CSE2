e8e3decbb90d94fab2901a21712816ba052bc5a6
yiw218 <yiw218@lehigh.edu>
2014-09-03 02:34:42 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+/////////////////////////////////////////
+//Yifan Wong
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[] args) {
+
+//  print the statement with box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-Y--I--W--2--1--8-|");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("v  v  v  v  v  v ");
+    System.out.println("This summer I went to Japan.");
+    System.out.println("The food there is fantastic.");
+    System.out.println("test");
+
+        //test
+    }
+}
